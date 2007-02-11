97fe8880db9296aea568f04f682e1e5e7f8e8bc9
jcspray <none@none>
2007-02-11 19:55:35 +0000

    symlink to changelog
    --HG--
    extra : convert_revision : svn%3A7d7b52a7-431a-0410-ad82-b695db389479/trunk%40290

--- www/upload.sh
+++ www/upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export IMAGES="*.png *.jpeg"
-export TEXT="*.html *.css"
+export TEXT="*.html *.css ChangeLog"
 export DOWNLOADS="downloads"
 
 rsync -L -r --rsh="ssh" ${IMAGES} ${TEXT} ${DOWNLOADS} icculus.org:/webspace/homepages/jcspray/referencer/
