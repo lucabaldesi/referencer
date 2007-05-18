d202b8544f308c4cedbdd1aaa240064c923040ac
jcspray <none@none>
2007-05-18 17:15:17 +0000

    stop uploading .svn stuff
    --HG--
    extra : convert_revision : svn%3A7d7b52a7-431a-0410-ad82-b695db389479/trunk%40458

--- www/upload.sh
+++ www/upload.sh
@@ -2,6 +2,6 @@
 
 export IMAGES="*.png *.ico"
 export TEXT="*.html *.css ChangeLog"
-export DOWNLOADS="downloads"
+export DOWNLOADS="downloads/*.tar.gz"
 
 rsync -v -L -r --rsh="ssh" ${IMAGES} ${TEXT} ${DOWNLOADS} icculus.org:/webspace/projects/referencer/
