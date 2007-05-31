d7c857230d8e541b54db5e650df94afc52b69136
jcspray <none@none>
2007-05-31 10:05:50 +0000

    1.0.4 release
    --HG--
    extra : convert_revision : svn%3A7d7b52a7-431a-0410-ad82-b695db389479/trunk%40518

--- www/upload.sh
+++ www/upload.sh
@@ -2,6 +2,6 @@
 
 export IMAGES="*.png *.ico"
 export TEXT="*.html *.css ChangeLog"
-export DOWNLOADS="downloads/*.tar.gz"
+export DOWNLOADS="downloads"
 
 rsync -v -L -r --rsh="ssh" ${IMAGES} ${TEXT} ${DOWNLOADS} icculus.org:/webspace/projects/referencer/
