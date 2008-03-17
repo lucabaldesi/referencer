9d7cc99e31d7f5bc91cbbc15cbccf6be86051f89
John Spray <jcspray@icculus.org>
2008-03-17 21:56:06 +0000

    note about patches and pulling

--- www/upload.sh
+++ www/upload.sh
@@ -2,6 +2,5 @@
 
 export IMAGES="*.png *.ico"
 export TEXT="*.html *.css ChangeLog"
-export DOWNLOADS="downloads"
 
-rsync -v -L -r --rsh="ssh" ${IMAGES} ${TEXT} ${DOWNLOADS} icculus.org:/webspace/projects/referencer/
+rsync -v -L -r --rsh="ssh" ${IMAGES} ${TEXT} icculus.org:/webspace/projects/referencer/
