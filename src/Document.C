0abc555793b60186d1c15f562331bc1bd27f39fe
Enno Gröper <enno@groeper-berlin.de>
2012-11-18 15:44:30 +1100

    Let referencer compile against libpoppler >= 0.15.0 Now page selection isn't needed anymore.

--- src/Document.C
+++ src/Document.C
@@ -528,6 +528,8 @@ bool Document::readPDF ()
 		#ifdef OLD_POPPLER
 			#warning Using poppler <= 0.5
 			textdump += poppler_page_get_text (page, rect);
+		#elif POPPLER_CHECK_VERSION(0, 15, 0)
+			textdump += poppler_page_get_text(page);
 		#else
 			#warning Using poppler >= 0.6
 			#ifdef HAVE_POPPLER_PAGE_GET_SELECTED_TEXT
